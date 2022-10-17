dcf081acee21e36db4911d8a48a0b630e3f507a8
Alisson Araujo Dos Santos <alisson.ads@hotmail.com>
2022-10-16 22:55:22 -0300

    splash page criada

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album/app/core/ui/styles/button_styles.dart';
+import 'package:fwc_album/app/core/ui/styles/colors_app.dart';
+import 'package:fwc_album/app/core/ui/styles/text_styles.dart';
+import 'package:fwc_album/app/core/ui/widgets/button.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -11,12 +15,52 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> with Loader {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Page'),
-      ),
-      body: const Center(
-        child: Text('Inicio do projeto'),
+      backgroundColor: context.colors.primary,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_splash.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: screenHeight * 0.08),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  'assets/images/fifa_logo.png',
+                  height: screenHeight * 0.25,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: screenHeight * 0.19),
+                child: Button(
+                  width: screenWidth * 0.9,
+                  onPressed: () {},
+                  style: context.buttonStyles.yellowButton,
+                  label: 'Acessar',
+                  labelStyle:
+                      context.textStyles.textSecondaryFontExtraBoldPrimaryColor,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Image.asset('assets/images/bandeiras.png'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
